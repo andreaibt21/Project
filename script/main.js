7315349f6936efc2739c288e5ba50fcddbc10c54
andreaibt21 <andreaixabella@gmail.com>
2020-08-04 20:09:13 -0300

    arreglo en monstruo y js de 2y4

--- script/main.js
+++ script/main.js
@@ -17,63 +17,6 @@ function Back_Ground(ide){
     Back_Ground("bg10");
     Back_Ground("bg14");
 
-let alien = document.getElementById("alien2");
-let alien4 = document.getElementById("alien4");
-let mensajeVerde = document.getElementsByClassName("verde")
-
-//-------------DOS
-alien.addEventListener("mouseenter", function(){
-    mensajeVerde[0].style.display = "block"
-})
-
-alien.addEventListener("mouseleave", function(){
-    mensajeVerde[0].style.display = "none"
-})
-
-//astro 
-
-let astronauta = document.getElementById("astro")
-let mensajeAzul= document.getElementsByClassName("mensajeA")
-
-
-astronauta.addEventListener("mouseenter", function(){
-    mensajeAzul[0].style.display = "block";
-    mensajeAzul[1].style.display = "block"
-})
-
-astronauta.addEventListener("mouseleave", function(){
-    mensajeAzul[0].style.display = "none";
-    mensajeAzul[1].style.display = "none"
-})
-
-
-//--------------CUATRO
-//alien
-alien4.addEventListener("mouseenter", function(){
-    mensajeVerde[1].style.display = "block"
-})
-
-alien4.addEventListener("mouseleave", function(){
-    mensajeVerde[1].style.display = "none"
-})
-
-//starlord
-
-let starLord = document.getElementById("starlord")
-let mensajeSL = document.getElementById("imagenTSL")
-let textoSL = document.getElementById("textoSL")
-
-starLord.addEventListener("mouseenter", function(){
-    mensajeSL.style.display = "block";
-    textoSL.style.display = "block";
-    
-})
-
-starLord.addEventListener("mouseleave", function(){
-    mensajeSL.style.display = "none";
-    textoSL.style.display = "none";
-
-})
 //J:Cambiar de ID para tener distintos diseños
 let divc=document.getElementById("seis")
 
