0471f28d90fb9f2af7a43d06fa6cb36811dfd43c
FlorenciaAilen <flormordcovici@gmail.com>
2020-08-12 09:37:47 -0300

    modal

--- script/main.js
+++ script/main.js
@@ -77,4 +77,24 @@ function modificar_id(ide){
 
 }
 
+//MODAL
+function modal(){
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+    
+    
+   
+      modal.style.display = "block";
+    
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
+}
 
